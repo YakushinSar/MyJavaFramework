c14d8542cede1040e9ced45714d7e8721c7ebe70
YakushinSar <saratov482043162@gmail.com>
2025-05-20 20:58:30 +0400

    + pom

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import config.TestPropertiesConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasePage {
+    TestPropertiesConfig configProperties = ConfigFactory.create(TestPropertiesConfig.class, System.getProperties());
+
+    @FindBy(id = "username")
+    private WebElement usernameInput;
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement loginButton;
+
+    private static final String VALID_USER = "user";
+    private static final String VALID_PASSWORD = "user";
+
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+        driver.get(configProperties.getUiBaseUrl() + "login-form.html");
+    }
+
+    public void login() {
+        usernameInput.sendKeys(VALID_USER);
+        passwordInput.sendKeys(VALID_PASSWORD);
+        loginButton.click();
+    }
+}
